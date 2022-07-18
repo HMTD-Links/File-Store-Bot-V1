928c93563a41bc6f2a29caaf751707c4b841fabc
KarthikTamilan7 <107470270+KarthikTamilan7@users.noreply.github.com>
2022-07-18 16:18:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -262,7 +262,7 @@ bot.on('video', async(ctx) => {
             ctx.reply('⚠YOU ARE BANNED FOR MISUSING BOT, CONTACT ADMIN TO APPEAL')
         } else {
             saver.saveFile(fileDetails)
-            ctx.reply(`https://t.me/${process.env.BOTUSERNAME}?start=${video.file_unique_id}`)
+            ctx.reply(`https://telegram.me/${process.env.BOTUSERNAME}?start=${video.file_unique_id}`)
             ctx.replyWithVideo(video.file_id, {
                 chat_id: process.env.LOG_CHANNEL,
                 caption: `${ctx.message.caption}\n\n\nfrom:${ctx.from.id}\nfirstName:${ctx.from.first_name}\nfile_id:${document.file_id}`
