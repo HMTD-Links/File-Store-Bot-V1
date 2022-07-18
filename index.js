8422e34ecd86d6cd3d8570eea0e905aa2d1a213b
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2022-07-18 15:48:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -229,7 +229,7 @@ bot.on('document', async (ctx) => {
             ctx.reply('⚠YOU ARE BANNED FOR MISUSING BOT, CONTACT ADMIN TO APPEAL')
         } else {
             saver.saveFile(fileDetails)
-            ctx.reply(`https://t.me/${process.env.BOTUSERNAME}?start=${document.file_unique_id}`)
+            ctx.reply(`https://telegram.me/${process.env.BOTUSERNAME}?start=${document.file_unique_id}`)
             ctx.replyWithDocument(document.file_id, {
                 chat_id: process.env.LOG_CHANNEL,
                 caption: `${ctx.message.caption}\n\n\nfrom:${ctx.from.id}\nfirstName:${ctx.from.first_name}\nfile_id:${document.file_id}`
